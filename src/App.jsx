1e48acc4e24c13f58c9a43a0f6e44b77a1f45ab5
Franciane Menestrina <francianemenestrina@gmail.com>
2022-09-11 11:57:47 -0300

    Iniando o portifolio

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+import Content from './routers/Content';
+
+function App() {
+  return (
+    <div>
+      <p>Oi</p>
+      <Content />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/Principal.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Principal() {
+  return (
+    <div>
+      <p>Página Principal</p>
+    </div>
+  );
+}
+
+export default Principal;
